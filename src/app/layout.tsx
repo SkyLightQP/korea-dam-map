e70a5a3938ca20e241d4176ba1a1c4f8e0d38327
Daegyeom Ha <combbm@gmail.com>
2025-07-09 00:56:35 +0900

    feat: `Pretendard GOV` 폰트 적용
    * l(소문자 엘), I(대문자 아이) 등과 같은 알파벳과 숫자 가독성 향상을 위해 기본 Pretendard 대신 Pretendard GOV를 사용합니다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import localFont from 'next/font/local';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
+const pretendardGov = localFont({
+  src: '../assets/font/PretendardGOVVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard-gov'
 });
 
 export const metadata: Metadata = {
@@ -23,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang="ko">
+      <body className={`${pretendardGov.variable} antialiased`}>{children}</body>
     </html>
   );
 }
